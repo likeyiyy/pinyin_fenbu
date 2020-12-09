1dd21d721e067c16aa9adca8d8a8330857ab70fa
Keyi.lee <keyi.lee@gllue.com>
2020-12-09 22:40:59 +0800

    :bugfix: [#] pinyinfenbu

--- analysis.py
+++ analysis.py
@@ -1,47 +1,74 @@
-from pyquery import PyQuery as pq
-import pymysql
+import json
+import os
 
+from peewee import MySQLDatabase, Model, CharField, IntegerField
+from pypinyin import pinyin, Style
+
+config_path = os.environ.get("CONFIG_PATH") or "/opt/web/config-pinyin.json"
+config_args = json.load(open(config_path, "r"))
+
+database = MySQLDatabase(**config_args)
+
+class Word(Model):
+    name = CharField()
+    pinyin = CharField()
+    sheng_diao = IntegerField()
+    level = IntegerField()
+
+    class Meta:
+        database = database
+
+def create_tables():
+    with database:
+        database.create_tables([Word])
+
+
+style = Style.TONE3
+
+result = []
 
 
 class Parser(object):
 
-    def __init__(self, file_path: str):
-        with open(file_path, "r") as fp:
-            html = fp.read()
-        self.doc = pq(html)
+    def __init__(self, path: str, level: int = 1):
+        self.path = path
+        self.level = level
+        self.result = []
+        with open(self.path, "r") as fp:
+            self.content = fp.read()
 
     def parse(self):
-        result = []
-        i = 0
-        pinyin_groups = self.doc.find(".box")
-        for idx, pinyin_group in enumerate(pinyin_groups):
-            for _sheng_diao in pq(pinyin_group).find(".box_list"):
-                sheng_diao = pq(_sheng_diao).find("label").text().replace("：", "")
-                for word in pq(_sheng_diao).find("div.aaa a"):
-                    result.append({
-                        "name": pinyin_group.find("a").text,
-                        "shengdiao": sheng_diao,
-                        "word": word.text
-                    })
-        return result
+        for word in self.content:
+            pinyins = pinyin(word, heteronym=True, style=style)[0]
+            for word_pinyin in pinyins:
+                if word_pinyin[-1].isdigit():
+                    sheng_diao = word_pinyin[-1]
+                    pinyin_name = word_pinyin[:-1]
+                else:
+                    sheng_diao = 0
+                    pinyin_name = word_pinyin
+                self.result.append(
+                    {
+                        "name": word,
+                        "pinyin": pinyin_name,
+                        "sheng_diao": sheng_diao,
+                        "level": self.level
+                    }
+                )
+                print(word, pinyin_name, sheng_diao, self.level)
 
-    def store(self, result):
-        db = pymysql.connect("mysql", "root", "gllue123", "pinyin")
-        cursor = db.cursor()
-        for item in result:
-            try:
-                # 执行sql语句
-                sql = f"insert into pinyin set `pinyin` = '{item['name']}', shengdiao = '{item['shengdiao']}', word = '{item['word']}';"
-                print(sql)
-                cursor.execute(sql)
-                # 提交到数据库执行
-                db.commit()
-            except Exception as e:
-                print(str(e))
-                # 如果发生错误则回滚
-                db.rollback()
+    def store(self):
+        if self.result:
+            import ipdb; ipdb.set_trace()
+            Word.insert_many(self.result).execute()
+
+    def parse_and_store(self):
+        self.parse()
+        self.store()
 
 
 if __name__ == "__main__":
-    parser = Parser("./pinyin_raw.html")
-    parser.store(parser.parse())
+    create_tables()
+    Parser("./word/level_1.txt", level=1).parse_and_store()
+    Parser("./word/level_2.txt", level=2).parse_and_store()
+    Parser("./word/level_3.txt", level=3).parse_and_store()
